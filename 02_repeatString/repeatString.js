9d4b8d28417c17b619070c181d69e550c02c4ae0
Kim <ktla3223@gmail.com>
2023-04-07 14:09:44 -0700

    Added lines 5-8

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,8 +2,9 @@ const repeatString = function(word, number) {
     if(number < 0) return "ERROR";
     let string = "";
     for (let i = 0; i < number; i++) {
-
+        string += word;
     }
+    return string;
 }
 
 // Do not edit below this line
