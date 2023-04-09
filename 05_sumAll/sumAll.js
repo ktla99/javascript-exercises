88ac59b60962b40474705125440c5d694ebe9a33
Kim <ktla3223@gmail.com>
2023-04-09 14:59:59 -0700

    Added line 5 for condiition if min > max

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,9 +2,11 @@ const sumAll = function(min, max) {
   if (min < 0 || max < 0) return "ERROR";
   if (isNaN(min) || isNaN(max)) return "ERROR";
   let finalSum = 0;
-  const backwards = min;
-  min = max;
-  max = backwards;
+  if (min > max) {
+    const backwards = min;
+    min = max;
+    max = backwards;
+  }
   for(let i = min; i < max + 1; i++) {
     finalSum += 1;
   }
