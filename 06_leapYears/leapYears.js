ab1be4e91ad44c8c8b98a4d7f0624fa023a19713
Kim <ktla3223@gmail.com>
2023-04-09 15:46:05 -0700

    Added Number.isInteger to line 2

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,8 @@
 const leapYears = function(year) {
-    if((year/4) && (!(year/100) || (year/400))) {
-        return true
+    if((Number.isInteger(year/4)) && (!(Number.isInteger((year/100)) || (Number.isInteger(year/400))))) {
+        return true;
     } else {
-        return false
+        return false;
     }
 };
 
