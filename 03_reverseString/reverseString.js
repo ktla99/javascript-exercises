e027413ccc17dd11de3d74e2b57637f01c229098
Kim <ktla3223@gmail.com>
2023-04-09 13:58:07 -0700

    Added lines 2-6 for vairables string, reversed, joined and return

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let string = phrase.split("");
+    let reversed = string.reverse();
+    let joined = reversed.join("");
+    return joined;
 };
 
 // Do not edit below this line
