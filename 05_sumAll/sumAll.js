d836b3b48fd04c7c5c74c96be6360206b81f0c0e
Kim <ktla3223@gmail.com>
2023-04-09 15:08:48 -0700

    Changed .isNaN() to Number.isInteger

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,15 @@
 const sumAll = function(min, max) {
   if (min < 0 || max < 0) return "ERROR";
-  if (isNaN(min) || isNaN(max)) return "ERROR";
-  let finalSum = 0;
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  
   if (min > max) {
     const backwards = min;
     min = max;
     max = backwards;
   }
-  for(let i = min; i < max + 1; i++) {
+
+  let finalSum = 0;
+  for (let i = min; i < max + 1; i++) {
     finalSum += 1;
   }
   return finalSum;
