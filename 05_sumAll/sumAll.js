8b84b509c52c7e7322b4f7328d4db4155c71b367
Kim <ktla3223@gmail.com>
2023-04-09 14:54:07 -0700

    Added lines 1-12:

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  if (min < 0 && max < 0) return "ERROR";
+  if (!isNaN(min) || !isNaN(max)) return "ERROR";
+  let finalSum = "";
+  const backwards = min;
+  min = max;
+  max = backwards;
+  if(let i = min; i < max + 1; i++) {
+    finalSum += backwards;
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
