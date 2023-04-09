dc69b6f7abbe3435b6c0bc45a4a68799816247b4
Kim <ktla3223@gmail.com>
2023-04-09 16:09:21 -0700

    Added lines 1-10 with Math.round

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp1) {
+  let celsius = (temp1 - 32)/1.8;
+  let rounded1 = Math.round(celsius * 10) / 10;
+  return rounded1;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp2) {
+  let fahrenheit = (temp * 1.8) + 32;
+  let rounded2 = Math.round(fahrenheit * 10) / 10;
+  return rounded2;
 };
 
 // Do not edit below this line
