4ffae9b77aa04f6fedd56eb69e063d7b0220bbdc
Kim <ktla3223@gmail.com>
2023-04-09 14:42:59 -0700

    Added lines 1-8 with .forEach, if, .include, .push and return

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(phrase, ...arg) {
+    let newPhrase = [];
+    phrase.forEach((item) => {
+        if(!arg.includes(item)) {
+            newPhrase.push(item);
+        }
+       });   return newPhrase;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
