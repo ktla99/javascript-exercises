7c7cb78bdd30cbfedfa81c0e84f3b54ccd117393
Kim <ktla3223@gmail.com>
2023-04-23 11:27:46 -0700

    Added lines 5-14 to check if punctuations are included

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,11 +2,18 @@ const palindromes = function (phrase) {
     let string = phrase.split("");
     let reversed = string.reverse();
     let joined = reversed.join("");
+    for(i = 0; i < joined.length; i ++) {
+        if(joined.charAt(i) == '!' || joined.charAt(i) == ',' || joined.charAt(i) == ';' || joined.charAt(i) == '.' ||  joined.charAt(i) == '?' || joined.charAt(i) == '-' ||    
+        joined.charAt(i) == '\'' || joined.charAt(i) == '\"' || joined.charAt(i) == ':') {
+            return joined = true;
+             break;
+        }
     if(phrase === joined) {
         return true;
     } else {
         return false;
     }
+}
 };
 
 // Do not edit below this line
