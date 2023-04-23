6c20fd7a72d83561d92187746eaee8021630e4da
Kim <ktla3223@gmail.com>
2023-04-23 11:07:03 -0700

    Changes + to * on line 25

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -20,9 +20,9 @@ const power = function(a, b) {
 
 const factorial = function(a) {
   if (a === 0 || a === 1) 
-  return 1;
+    return 1;
   for (i = a - 1; i >= 1; i--) {
-    a += i;
+    a *= i;
   }
   return a;
 }
