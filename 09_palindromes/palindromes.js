3e79136f43942e9e92d88707bd4cff0fcde96233
Kim <ktla3223@gmail.com>
2023-04-23 11:13:21 -0700

    Added lines 1-8 to reverse the input phrase

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    let string = phrase.split("");
+    let reversed = string.reverse();
+    let joined = reversed.join("");
+    if(phrase === joined) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
