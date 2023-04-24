e1b53817645013ffd844c6062f22fc0af5475f01
Kim <ktla3223@gmail.com>
2023-04-24 10:48:22 -0700

    Added function to fibonacci on lines 1-8

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    const firstNum = 0;
+    if(a < 0)
+        return "Error";
+    for (i = a; i > a + 1; i++) {
+        firstNum += a[i];
+    }
+    return firstNum;
 };
 
 // Do not edit below this line
