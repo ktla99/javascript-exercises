3f4cea32528c90b457662f385c5cdcf99192fb56
Kim <ktla3223@gmail.com>
2023-04-24 12:35:59 -0700

    Added function for finding person who is still alive and the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,22 @@
-const findTheOldest = function() {
-    const age = people.reduce((total, person) => {
-        return total + (people.yearOfDeath - people.yearOfBirth);
-        }, 0);    
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge (
+            currentPerson.yearOfBirth,
+            currentPerson.yearOfDeath
+        );
+        return oldestAge < currentAge ? currentAge : oldest;
+    });
 };
+
+const getAge = function(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+};
+
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
