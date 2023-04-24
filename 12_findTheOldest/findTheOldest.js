692ba44d716db5447b245c389312df0b28705628
Kim <ktla3223@gmail.com>
2023-04-24 12:07:14 -0700

    Created a function to determine the oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
 const findTheOldest = function() {
-
+    const age = people.reduce((total, person) => {
+        return total + (people.yearOfDeath - people.yearOfBirth);
+        }, 0);    
 };
 
 // Do not edit below this line
