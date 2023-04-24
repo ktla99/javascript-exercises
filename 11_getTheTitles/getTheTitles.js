7d14529987c6840e47a1792fac099c2f3ab39460
Kim <ktla3223@gmail.com>
2023-04-24 11:41:59 -0700

    Attempted to get the titles of the books with the function on lines 1-11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(title, author) {
+    let titles = '';
+    let authors = '';
+    const books = [
+        {
+            title: titles,
+            author: authors
+        },
+        {
+            title: titles,
+            author: authors
+        }
+    ]
+     
 };
 
 // Do not edit below this line
